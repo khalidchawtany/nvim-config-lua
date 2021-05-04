9176a950682692f54e9706cc02eb1e3314cafd5c
KhalidChawtany <khalidchawtany@yahoo.com>
2021-05-04 14:13:54 +0300

    WIP: php lsp

--- lua/_lsp.lua
+++ lua/_lsp.lua
@@ -1,11 +1,4 @@
-
-
-
-
-
-
 -- LUA
-
 -- https://github.com/sumneko/lua-language-server/wiki/Build-and-Run-(Standalone)
 USER = vim.fn.expand('$USER')
 
@@ -13,11 +6,11 @@ local sumneko_root_path = ""
 local sumneko_binary = ""
 
 if vim.fn.has("mac") == 1 then
-    sumneko_root_path = "/Users/" .. USER .. "/.config/nvim/lua-language-server"
-    sumneko_binary = "/Users/" .. USER .. "/.config/nvim/lua-language-server/bin/macOS/lua-language-server"
+    sumneko_root_path = "/Users/" .. USER .. "/.config/nvim/lsp/lua-language-server"
+    sumneko_binary = "/Users/" .. USER .. "/.config/nvim/lsp/lua-language-server/bin/macOS/lua-language-server"
 elseif vim.fn.has("unix") == 1 then
-    sumneko_root_path = "/home/" .. USER .. "/.config/nvim/lua-language-server"
-    sumneko_binary = "/home/" .. USER .. "/.config/nvim/lua-language-server/bin/Linux/lua-language-server"
+    sumneko_root_path = "/home/" .. USER .. "/.config/nvim/lsp/lua-language-server"
+    sumneko_binary = "/home/" .. USER .. "/.config/nvim/lsp/lua-language-server/bin/Linux/lua-language-server"
 else
     print("Unsupported system for sumneko")
 end
@@ -60,3 +53,6 @@ require"lspconfig".efm.setup {
     }
 }
 vim.cmd([[ autocmd BufWritePre *.lua lua vim.lsp.buf.formatting_sync(nil, 100) ]])
+
+-- PHP
+require'lspconfig'.phpactor.setup {}
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,59 +1,56 @@
-return require('packer').startup(function()
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  -- Utilities
-  -- Mapping
-  use { 'svermeulen/vimpeccable' }
+    -- Utilities
+    -- Mapping
+    use {'svermeulen/vimpeccable'}
 
-  -- LSP
-  use {'neovim/nvim-lspconfig'}
-  use {'ray-x/navigator.lua', requires = {'ray-x/guihua.lua', run = 'cd lua/fzy && make'}}
+    -- LSP
+    use {'neovim/nvim-lspconfig'}
+    use {'ray-x/navigator.lua', requires = {'ray-x/guihua.lua', run = 'cd lua/fzy && make'}}
 
-  -- Treesitter
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use { 'nvim-treesitter/nvim-treesitter-refactor' }
+    -- Treesitter
+    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+    use {'nvim-treesitter/nvim-treesitter-refactor'}
 
+    -- Completion
+    use {'nvim-lua/completion-nvim'}
 
-  -- Completion
-  use { 'nvim-lua/completion-nvim' }
+    -- File Browser
+    use {'tamago324/lir.nvim', requires = {'kyazdani42/nvim-web-devicons', 'nvim-lua/plenary.nvim'}}
+    use {'tamago324/lir-bookmark.nvim', requires = {'tamago324/lir.nvim'}}
 
+    use {'nvim-lua/telescope.nvim', requires = {'nvim-lua/plenary.nvim', 'nvim-lua/popup.nvim'}}
 
-  -- File Browser
-  use { 'tamago324/lir.nvim', requires = {'kyazdani42/nvim-web-devicons', 'nvim-lua/plenary.nvim'} }
-  use { 'tamago324/lir-bookmark.nvim', requires = {'tamago324/lir.nvim'} }
+    -- Git
+    use {
+        'lewis6991/gitsigns.nvim',
+        requires = {'nvim-lua/plenary.nvim'},
+        config = function()
+            require('gitsigns').setup()
+        end
+    }
 
-  use { 'nvim-lua/telescope.nvim', requires = { 'nvim-lua/plenary.nvim', 'nvim-lua/popup.nvim' } }
+    -- Autopairs
+    use {'windwp/nvim-autopairs'}
 
-  -- Git
-  use {
-    'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
-    config = function() require('gitsigns').setup() end
-  }
+    -- Theme
+    use {
+        'glepnir/galaxyline.nvim',
+        branch = 'main',
+        -- your statusline
+        config = function()
+            require '_galaxyline'
+        end,
+        -- some optional icons
+        requires = {'kyazdani42/nvim-web-devicons', opt = true}
+    }
 
+    use {"folke/which-key.nvim"}
 
-  -- Autopairs
-  use { 'windwp/nvim-autopairs' }
-
-
-  -- Theme
-  use {
-    'glepnir/galaxyline.nvim',
-      branch = 'main',
-      -- your statusline
-      config = function() require'_galaxyline' end,
-      -- some optional icons
-      requires = {'kyazdani42/nvim-web-devicons', opt = true}
-  }
-
-use { "folke/which-key.nvim" }
-
-
--- Global remapping
-------------------------------
-require('telescope').setup{
-  defaults = {
-  }
-}
+    -- Global remapping
+    ------------------------------
+    require('telescope').setup {defaults = {}}
 
 end)
