b4f342c637b07c7b01b51ec8c1f02ce58d74af62
KhalidChawtany <khalidchawtany@yahoo.com>
2021-09-13 16:30:32 +0300

    wip

--- lua/configs/snap.lua
+++ lua/configs/snap.lua
@@ -1,10 +1,24 @@
 local M = {
-  keys = {"<c-p><-0>"},
   module = "snap"
 }
 
+M.init = function()
+  vim.cmd [[
+    nnoremap  <silent> <C-space><c-p> <cmd>lua require('configs/snap').fzf()<cr>
+    nnoremap  <silent> <C-space><c-o> <cmd>lua require('configs/snap').buffer()<cr>
+    nnoremap  <silent> <C-space><c-u> <cmd>lua require('configs/snap').oldfile()<cr>
+    nnoremap  <silent> <C-space><c-l> <cmd>lua require('configs/snap').luvfile()<cr>
+  ]]
+end
+
 M.config = function()
-  vim.cmd [[ nnoremap  <silent> <C-p><c-.> <cmd>lua require('configs/snap').fzf()<cr> ]]
+  -- local snap = require("snap")
+  -- snap.maps {
+  --   {"<c-space>p", snap.config.file {producer = "ripgrep.file"}},
+  --   {"<c-space>o", snap.config.file {producer = "vim.buffer"}},
+  --   {"<c-space>u", snap.config.file {producer = "vim.oldfile"}},
+  --   {"<c-space>j", snap.config.vimgrep {}}
+  -- }
 end
 
 M.fzf = function()
@@ -16,6 +30,30 @@ M.fzf = function()
     select = snap.get "select.file".select,
     multiselect = snap.get "select.file".multiselect,
     views = {snap.get "preview.file"}
+  }
+end
+
+M.buffer = function()
+  local snap = require("snap")
+  snap.run {
+    producer = snap.get "consumer.fzf"(snap.get "producer.vim.buffer"),
+    select = snap.get "select.file".select
+  }
+end
+
+M.oldfile = function()
+  local snap = require("snap")
+  snap.run {
+    producer = snap.get "consumer.fzf"(snap.get "producer.vim.oldfile"),
+    select = snap.get "select.file".select
+  }
+end
+
+M.luvfile = function()
+  local snap = require("snap")
+  snap.run {
+    producer = snap.get "consumer.fzf"(snap.get "producer.luv.file"),
+    select = snap.get "select.file".select
   }
 end
 
