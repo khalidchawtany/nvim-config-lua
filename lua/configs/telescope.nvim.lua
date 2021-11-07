f94862318d9e1a77db1c86d13381d64ee109c7dc
KhalidChawtany <khalidchawtany@yahoo.com>
2021-11-07 09:40:02 +0300

    improve telescope layout_config

--- lua/configs/telescope.nvim.lua
+++ lua/configs/telescope.nvim.lua
@@ -1,15 +1,15 @@
 local M = {
-    requires = {
-        'nvim-lua/plenary.nvim',
-        'nvim-lua/popup.nvim',
-        'nvim-telescope/telescope-fzy-native.nvim'
-    },
-    module={'telescope'},
-    -- cmd = {'Telescope'},
+  requires = {
+    "nvim-lua/plenary.nvim",
+    "nvim-lua/popup.nvim",
+    "nvim-telescope/telescope-fzy-native.nvim"
+  },
+  module = {"telescope"}
+  -- cmd = {'Telescope'},
 }
 
 M.init = function()
-    vim.cmd [[
+  vim.cmd [[
             nnoremap <c-s><c-p>  <cmd>lua require('telescope.builtin').find_files()<cr>
             nnoremap <c-s>p      <cmd>lua require('telescope.builtin').find_files({find_command = {"fd", vim.fn.expand("<cword>")}})<cr>
             nnoremap <c-s><c-a>  <cmd>lua require('telescope.builtin').live_grep()<cr>
@@ -26,100 +26,115 @@ M.init = function()
             nnoremap <c-s><c-e>  <cmd>lua require('telescope.builtin').file_browser()<cr>
     ]]
 
-    vim.api.nvim_set_keymap('n', '<C-p><c-\\>', ":lua require'telescope'.extensions.project.project{}<CR>", {noremap = true, silent = true})
+  vim.api.nvim_set_keymap(
+    "n",
+    "<C-p><c-\\>",
+    ":lua require'telescope'.extensions.project.project{}<CR>",
+    {noremap = true, silent = true}
+  )
 
-    vim.api.nvim_set_keymap('n', '<c-s><c-d>', ":lua require('telescope.builtin').lsp_definitions()<CR>", {noremap = true, silent = true})
+  vim.api.nvim_set_keymap(
+    "n",
+    "<c-s><c-d>",
+    ":lua require('telescope.builtin').lsp_definitions()<CR>",
+    {noremap = true, silent = true}
+  )
 
-    -- vim.api.nvim_set_keymap('n', '<c-p>p', ":lua require('telescope.builtin').find_files{search = vim.fn.expand(\"<cword>\") }<CR>",
-    --                        {noremap = true, silent = true})
-
+  -- vim.api.nvim_set_keymap('n', '<c-p>p', ":lua require('telescope.builtin').find_files{search = vim.fn.expand(\"<cword>\") }<CR>",
+  --                        {noremap = true, silent = true})
 end
 
 M.config = function()
-    -- in find files avoid normal mode
+  -- in find files avoid normal mode
 
-    local actions = require('telescope.actions')
-    require('telescope').setup {
-        defaults = {
-            mappings = {
-                i = {
-                    -- To disable a keymap, put [map] = false
-                    -- So, to not map "<C-n>", just put
-                    ["<c-x>"] = false,
-                    -- ["<esc>"] = actions.close,
+  local actions = require("telescope.actions")
+  require("telescope").setup {
+    defaults = {
+      mappings = {
+        i = {
+          -- To disable a keymap, put [map] = false
+          -- So, to not map "<C-n>", just put
+          ["<c-x>"] = false,
+          -- ["<esc>"] = actions.close,
 
-                    -- Otherwise, just set the mapping to the function that you want it to be.
-                    ["<C-i>"] = actions.select_horizontal,
-                    ["<C-j>"] = actions.move_selection_next,
-                    ["<C-k>"] = actions.move_selection_previous,
-                    ["<c-space>"] = actions.toggle_selection + actions.move_selection_better
+          -- Otherwise, just set the mapping to the function that you want it to be.
+          ["<C-i>"] = actions.select_horizontal,
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+          ["<c-space>"] = actions.toggle_selection + actions.move_selection_better
 
-                    -- Add up multiple actions
-                    -- ["<CR>"] = actions.select_default + actions.center
+          -- Add up multiple actions
+          -- ["<CR>"] = actions.select_default + actions.center
 
-                    -- You can perform as many actions in a row as you like
-                    -- ["<CR>"] = actions.select_default + actions.center + my_cool_custom_action,
-                },
-                n = {
-                    ["<esc>"] = actions.close
-                    -- ["<C-i>"] = my_cool_custom_action,
-                }
-            },
-
-            vimgrep_arguments = {'rg', '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case'},
-            prompt_prefix = "> ",
-            selection_caret = "> ",
-            entry_prefix = "  ",
-            initial_mode = "insert",
-            selection_strategy = "reset",
-            sorting_strategy = "descending",
-            file_sorter = require'telescope.sorters'.get_fuzzy_file,
-            file_ignore_patterns = {},
-            generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
-            -- path_display = {},
-            winblend = 0,
-            layout_strategy = "vertical",
-            -- layout_defaults = {horizontal = {mirror = false}, vertical = {mirror = false}},
-            layout_config = {
-                horizontal = {mirror = false},
-                vertical = {mirror = false},
-                width = 0.8,
-                height = 0.9,
-                -- preview_width = 0.4,
-                prompt_position = "bottom",
-                preview_cutoff = 10
-            },
-            border = {},
-            borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
-            color_devicons = true,
-            use_less = true,
-            set_env = {['COLORTERM'] = 'truecolor'}, -- default = nil,
-            file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-            grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-            qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
-
-            -- Developer configurations: Not meant for general override
-            buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+          -- You can perform as many actions in a row as you like
+          -- ["<CR>"] = actions.select_default + actions.center + my_cool_custom_action,
+        },
+        n = {
+          ["<esc>"] = actions.close
+          -- ["<C-i>"] = my_cool_custom_action,
         }
+      },
+      vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case"
+      },
+      prompt_prefix = "> ",
+      selection_caret = "> ",
+      entry_prefix = "  ",
+      initial_mode = "insert",
+      selection_strategy = "reset",
+      sorting_strategy = "descending",
+      file_sorter = require "telescope.sorters".get_fuzzy_file,
+      file_ignore_patterns = {},
+      generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
+      -- path_display = {},
+      winblend = 0,
+      layout_strategy = "vertical",
+      -- layout_defaults = {horizontal = {mirror = false}, vertical = {mirror = false}},
+      layout_config = {
+        horizontal = {mirror = false},
+        vertical = {
+          mirror = false,
+          preview_height = 0.3
+        },
+        width = 0.8,
+        height = 0.95,
+        -- preview_width = 0.4,
+        prompt_position = "bottom",
+        preview_cutoff = 10
+      },
+      border = {},
+      borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
+      color_devicons = true,
+      use_less = true,
+      set_env = {["COLORTERM"] = "truecolor"}, -- default = nil,
+      file_previewer = require "telescope.previewers".vim_buffer_cat.new,
+      grep_previewer = require "telescope.previewers".vim_buffer_vimgrep.new,
+      qflist_previewer = require "telescope.previewers".vim_buffer_qflist.new,
+      -- Developer configurations: Not meant for general override
+      buffer_previewer_maker = require "telescope.previewers".buffer_previewer_maker
     }
+  }
 
-    require('telescope').setup {
-        extensions = {
-            fzy_native = {override_generic_sorter = false, override_file_sorter = true},
-
-            fzf = {
-                fuzzy = true, -- false will only do exact matching
-                override_generic_sorter = false, -- override the generic sorter
-                override_file_sorter = true, -- override the file sorter
-                case_mode = "smart_case" -- or "ignore_case" or "respect_case"
-                -- the default case_mode is "smart_case"
-            }
-        }
+  require("telescope").setup {
+    extensions = {
+      fzy_native = {override_generic_sorter = false, override_file_sorter = true},
+      fzf = {
+        fuzzy = true, -- false will only do exact matching
+        override_generic_sorter = false, -- override the generic sorter
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "smart_case" -- or "ignore_case" or "respect_case"
+        -- the default case_mode is "smart_case"
+      }
     }
-    require('telescope').load_extension('fzy_native')
-    require('telescope').load_extension('fzf')
-
+  }
+  require("telescope").load_extension("fzy_native")
+  require("telescope").load_extension("fzf")
 end
 
 return M
-
