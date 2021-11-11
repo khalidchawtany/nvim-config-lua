e8a422af00ebb3efa0f9209ecd70c7ddfa7189f9
KhalidChawtany <khalidchawtany@yahoo.com>
2021-11-11 23:28:30 +0300

    disable stabilize for now

--- lua/configs/stabilize.nvim.lua
+++ lua/configs/stabilize.nvim.lua
@@ -1,11 +1,14 @@
-local M = {}
+local M = {
+  disabled = true
+}
 
 M.config = function()
   require("stabilize").setup(
     {
       force = true, -- stabilize window even when current cursor position will be hidden behind new window
       forcemark = nil, -- set context mark to register on force event which can be jumped to with '<forcemark>
-      ignore = { -- do not manage windows matching these file/buftypes
+      ignore = {
+        -- do not manage windows matching these file/buftypes
         filetype = {"help", "list", "Trouble", "fugitive", "gitcommit"},
         buftype = {"terminal", "quickfix", "loclist"}
       },
