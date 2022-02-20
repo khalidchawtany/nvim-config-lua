d3bb7177c147c46b2d71d8e53f0b0333239d4ca4
KhalidChawtany <khalidchawtany@yahoo.com>
2022-02-21 00:42:53 +0300

    remove better white space operator

--- lua/configs/vim-better-whitespace.lua
+++ lua/configs/vim-better-whitespace.lua
@@ -2,6 +2,7 @@ local M= {}
 
 M.init = function ()
   vim.cmd [[
+	let g:better_whitespace_operator = ''
   let g:better_whitespace_guicolor='WarnColor'
   let g:better_whitespace_filetypes_blacklist = [ 'diff', 'git', 'gitcommit', 'qf', 'help', 'fugitive', 'dashboard' ]
   ]]
