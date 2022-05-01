a1f94d85e4b5004afede49ef6243c12cbde6817d
KhalidChawtany <khalidchawtany@yahoo.com>
2022-05-01 17:07:27 +0300

    fix pretty-fold

--- lua/configs/pretty-fold.nvim.lua
+++ lua/configs/pretty-fold.nvim.lua
@@ -1,6 +1,8 @@
-local M = {}
+local M = {
+   requires = 'anuvyklack/nvim-keymap-amend' -- only for preview
+}
 M.config = function()
   require("pretty-fold").setup {}
-  require("pretty-fold.preview").setup_keybinding()
+  require('pretty-fold.preview').setup()
 end
 return M
