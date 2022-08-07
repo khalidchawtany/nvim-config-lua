dc6dec344c888c9310b7805c068da53c2296d8cb
KhalidChawtany <khalidchawtany@yahoo.com>
2022-08-07 10:01:03 +0300

    add callbacks to nomodoro

--- lua/configs/nomodoro.lua
+++ lua/configs/nomodoro.lua
@@ -11,12 +11,11 @@ local M = {
 
 M.config = function()
   local map = vim.api.nvim_set_keymap
-  local opts = { noremap = true, silent = true }
+  local opts = {noremap = true, silent = true}
 
-  map('n', '<leader>nw', '<cmd>NomoWork<cr>', opts)
-  map('n', '<leader>nb', '<cmd>NomoBreak<cr>', opts)
-  map('n', '<leader>ns', '<cmd>NomoStop<cr>', opts)
-
+  map("n", "<leader>nw", "<cmd>NomoWork<cr>", opts)
+  map("n", "<leader>nb", "<cmd>NomoBreak<cr>", opts)
+  map("n", "<leader>ns", "<cmd>NomoStop<cr>", opts)
 
   require("nomodoro").setup(
     {
@@ -28,7 +27,25 @@ M.config = function()
         on_work_complete = "TIME IS UP!",
         status_icon = "羽",
         timer_format = "!%0M:%0S" -- To include hours: '!%0H:%0M:%0S'
-      }
+      },
+      on_work_complete = function()
+        vim.fn.jobstart(
+          {"afplay", "/Users/juju/Development/Sounds/Bebebebeeep.mp3"},
+          {
+            on_stdout = function(j, d, e)
+            end
+          }
+        )
+      end,
+      on_break_complete = function()
+        vim.fn.jobstart(
+          {"afplay", "/Users/juju/Development/Sounds/tic_short.mp3"},
+          {
+            on_stdout = function(j, d, e)
+            end
+          }
+        )
+      end
     }
   )
 end
