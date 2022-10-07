66df3b94da462b35c7bc17b8a0733ae910bb552a
KhalidChawtany <khalidchawtany@yahoo.com>
2022-10-07 13:30:56 +0300

    add notify condif

--- lua/configs/nvim-notify.lua
+++ lua/configs/nvim-notify.lua
@@ -1,5 +1,23 @@
 local M = {}
 M.config = function()
-  require("notify").setup()
+  require("notify").setup(
+      {
+        background_colour = "Normal",
+        fps = 30,
+        icons = {
+          DEBUG = "",
+          ERROR = "",
+          INFO = "",
+          TRACE = "✎",
+          WARN = ""
+        },
+        level = 2,
+        minimum_width = 50,
+        render = "default",
+        stages = "static", -- fade_in_slide_out, fade, slide, static
+        timeout = 3000,
+        top_down = true
+      }
+  )
 end
 return M
