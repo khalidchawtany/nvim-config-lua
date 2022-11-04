bceeca06adb53f68334f4ef4d51e1e5250dbc974
KhalidChawtany <khalidchawtany@yahoo.com>
2022-11-04 13:41:08 +0300

    initialize neorg

--- lua/configs/neorg.lua
+++ lua/configs/neorg.lua
@@ -1,4 +1,29 @@
-local M = {ft = {'norg'}}
+local M = {
+  ft = {"norg"},
+  -- tag = "*",
+  after = "nvim-treesitter" -- You may want to specify Telescope here as well
+}
+
+M.config = function()
+  require("neorg").setup {
+    load = {
+      ["core.defaults"] = {},
+      ["core.norg.concealer"] = {
+        config = {
+          icon_preset = "diamond"
+        }
+      },
+      ["core.norg.completion"] = {
+        config = {}
+      },
+      ["core.export"] = {
+        config = {}
+      },
+      ["core.export.markdown"] = {
+        config = {}
+      }
+    }
+  }
+end
 
 return M
-
