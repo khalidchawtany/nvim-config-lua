afbe274b1aa4e6fa7b1179a24358d5c08f4fd080
KhalidChawtany <khalidchawtany@yahoo.com>
2023-01-23 14:20:59 +0300

    fix xptemplate mapping <c-j>

--- lua/configs/xptemplate.lua
+++ lua/configs/xptemplate.lua
@@ -3,8 +3,8 @@ local M = {event = {'InsertEnter'}}
 M.config = function()
     vim.cmd [[set runtimepath+=/Users/juju/.config/nvim/xpt-personal]]
     vim.g.xptemplate_key = '<c-\\>'
-    vim.g.xptemplate_nav_next = '<C-j>'
-    vim.g.xptemplate_nav_prev = '<C-k>'
+    vim.g.xptemplate_nav_next = '<D-j>'
+    vim.g.xptemplate_nav_prev = '<D-k>'
     vim.g.xptemplate_minimal_prefix = 0
 end
 
