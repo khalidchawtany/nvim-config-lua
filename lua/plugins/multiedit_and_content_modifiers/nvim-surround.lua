124227f9baa6f53665593a36e24b6383fc07a7ea
KhalidChawtany <khalidchawtany@yahoo.com>
2023-01-23 14:37:54 +0300

    remove nvim-surround

--- lua/plugins/multiedit_and_content_modifiers/nvim-surround.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "kylechui/nvim-surround",
-  keys = {
-    {"ys", "ds", "ds", {"S", mode = "v"}, {"<c-g>s", mode = "i"}}
-  },
-  config = function()
-    require("nvim-surround").setup({})
-  end
-}
