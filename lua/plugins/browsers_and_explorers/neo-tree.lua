b5eb1543f62bf325b2e3119d1e0528a7640d7cb8
KhalidChawtany <khalidchawtany@yahoo.com>
2023-03-03 09:16:07 +0300

    implement neo-tree global copy paste

--- lua/plugins/browsers_and_explorers/neo-tree.lua
+++ lua/plugins/browsers_and_explorers/neo-tree.lua
@@ -1,3 +1,5 @@
+_G.neotree_global_clipboard = {}
+
 local M = {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v2.x",
@@ -8,28 +10,31 @@ local M = {
     "MunifTanjim/nui.nvim"
   },
   keys = {
-      { "--", '<cmd>execute"Neotree float reveal dir=".expand("%:h")." reveal_force_cwd"<cr>', desc = "NeoTree (float"},
-      { "-r", "<cmd>Neotree right reveal<cr>", desc = "NeoTree (Reveal right)"},
-      { "-l", "<cmd>Neotree left reveal<cr>", desc = "NeoTree (Reveal left)"},
-      { "-f", "<cmd>Neotree float reveal<cr>", desc = "NeoTree (Reveal float)"},
-      { "-g", "<cmd>Neotree float git_status<cr>", desc = "NeoTree (Git status)"},
-      { "-b", "<cmd>Neotree float buffers<cr>", desc = "NeoTree (Buffers)"},
-      { "-<space>", "<cmd>Neotree float buffers<cr>", desc = "NeoTree (Buffers)"},
-      { "<space>-", "<cmd>Neotree float buffers<cr>", desc = "NeoTree (Buffers)"},
-      { "-0", '<cmd>execute"Neotree float dir=".FindGitDirOrRoot()<cr>', desc = "NeoTree (Root)"},
+    {
+      "--",
+      '<cmd>execute"Neotree float reveal dir=".expand("%:h")." reveal_force_cwd"<cr>',
+      desc = "NeoTree (float"
     },
+    {"-r", "<cmd>Neotree right reveal<cr>", desc = "NeoTree (Reveal right)"},
+    {"-l", "<cmd>Neotree left reveal<cr>", desc = "NeoTree (Reveal left)"},
+    {"-f", "<cmd>Neotree float reveal<cr>", desc = "NeoTree (Reveal float)"},
+    {"-g", "<cmd>Neotree float git_status<cr>", desc = "NeoTree (Git status)"},
+    {"-b", "<cmd>Neotree float buffers<cr>", desc = "NeoTree (Buffers)"},
+    {"-<space>", "<cmd>Neotree float buffers<cr>", desc = "NeoTree (Buffers)"},
+    {"<space>-", "<cmd>Neotree float buffers<cr>", desc = "NeoTree (Buffers)"},
+    {"-0", '<cmd>execute"Neotree float dir=".FindGitDirOrRoot()<cr>', desc = "NeoTree (Root)"}
+  }
   -- cmd = {"Neotree"},
 }
 
-
 M.init = function()
-	vim.g.neo_tree_remove_legacy_commands = 1
-	if vim.fn.argc() == 1 then
-		local stat = vim.loop.fs_stat(vim.fn.argv(0))
-		if stat and stat.type == "directory" then
-			require("neo-tree")
-		end
-	end
+  vim.g.neo_tree_remove_legacy_commands = 1
+  if vim.fn.argc() == 1 then
+    local stat = vim.loop.fs_stat(vim.fn.argv(0))
+    if stat and stat.type == "directory" then
+      require("neo-tree")
+    end
+  end
 end
 
 M.config = function()
@@ -65,12 +70,12 @@ M.config = function()
               local node = state.tree:get_node()
 
               if state.path == node.path or state.path == node:get_parent_id() then
-                require "neo-tree.ui.renderer".focus_node(state, node:get_parent_id())
+                require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
                 state.commands["navigate_up"](state)
                 return
               end
 
-              require "neo-tree.ui.renderer".focus_node(state, node:get_parent_id())
+              require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
               state.commands["toggle_directory"](state)
             end,
             ["."] = "set_root",
@@ -80,21 +85,35 @@ M.config = function()
             ["/"] = "fuzzy_finder",
             ["f"] = "filter_on_submit",
             ["<c-x>"] = "clear_filter",
+            ["y"] = function(state)
+              -- local cc = require("neo-tree.sources.common.commands")
+              local fs_cmds = require("neo-tree.sources.filesystem.commands")
+              fs_cmds.copy_to_clipboard(state)
+              table_merge(_G.neotree_global_clipboard, state.clipboard)
+            end,
+            ["p"] = function(state)
+              -- local cc = require("neo-tree.sources.common.commands")
+              local fs_cmds = require("neo-tree.sources.filesystem.commands")
+			  state.clipboard = state.clipboard or {}
+              table_merge(state.clipboard, _G.neotree_global_clipboard)
+              fs_cmds.paste_from_clipboard(state)
+              _G.neotree_global_clipboard = {}
+            end,
             ["h"] = function(state)
               local node = state.tree:get_node()
               if node.type == "directory" and node:is_expanded() then
-                require "neo-tree.sources.filesystem".toggle_directory(state, node)
+                require("neo-tree.sources.filesystem").toggle_directory(state, node)
               else
-                require "neo-tree.ui.renderer".focus_node(state, node:get_parent_id())
+                require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
               end
             end,
             ["l"] = function(state)
               local node = state.tree:get_node()
               if node.type == "directory" then
                 if not node:is_expanded() then
-                  require "neo-tree.sources.filesystem".toggle_directory(state, node)
+                  require("neo-tree.sources.filesystem").toggle_directory(state, node)
                 elseif node:has_children() then
-                  require "neo-tree.ui.renderer".focus_node(state, node:get_child_ids()[1])
+                  require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
                 end
               end
             end
