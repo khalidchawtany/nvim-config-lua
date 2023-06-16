40f88944f8e9a6e6fc64915bf1d03732fc211be0
KhalidChawtany <khalidchawtany@yahoo.com>
2023-06-17 00:26:49 +0300

    added edby and dropbar-nvim

--- lua/plugins/ui_and_themes/dropbar-nvim.lua
+++ lua/plugins/ui_and_themes/dropbar-nvim.lua
@@ -1,1 +1,60 @@
-return { "Bekaboo/dropbar.nvim" }
+return {
+	"Bekaboo/dropbar.nvim",
+	event = "VeryLazy",
+
+	dependencies = {
+		"kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+	},
+	keys = {
+		{
+			"s<cr>",
+			function()
+				require("dropbar.api").pick()
+			end,
+			desc = "Dropbar pick",
+		},
+	},
+	opts = {
+		menu = {
+			keymaps = {
+				["<LeftMouse>"] = function()
+					local api = require("dropbar.api")
+					local menu = api.get_current_dropbar_menu()
+					if not menu then
+						return
+					end
+					local mouse = vim.fn.getmousepos()
+					if mouse.winid ~= menu.win then
+						local prev_menu = api.get_dropbar_menu(mouse.winid)
+						if prev_menu and prev_menu.sub_menu then
+							prev_menu.sub_menu:close()
+						end
+						if vim.api.nvim_win_is_valid(mouse.winid) then
+							vim.api.nvim_set_current_win(mouse.winid)
+						end
+						return
+					end
+					menu:click_at({ mouse.line, mouse.column - 1 }, nil, 1, "l")
+				end,
+				["<CR>"] = function()
+					local menu = require("dropbar.api").get_current_dropbar_menu()
+					if not menu then
+						return
+					end
+					local cursor = vim.api.nvim_win_get_cursor(menu.win)
+					local component = menu.entries[cursor[1]]:first_clickable(cursor[2])
+					if component then
+						menu:click_on(component, nil, 1, "l")
+					end
+				end,
+				["q"] = function()
+					local menu = require("dropbar.api").get_current_dropbar_menu()
+					if not menu then
+						return
+					end
+					menu:close()
+				end,
+			},
+		},
+	},
+}
--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -40,31 +40,31 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 	end,
 })
 
-local winbarAutoCommandId = nil
-local setUpWinbarAutoCmd = function()
-	winbarAutoCommandId = vim.api.nvim_create_autocmd(
-		{ "CursorMoved", "BufWinEnter", "BufFilePost", "InsertEnter", "BufWritePost" },
-		{
-			callback = function()
-				require("user.winbar").get_winbar()
-			end,
-		}
-	)
-end
-
--- if not vim.fn.has('gui_vimr') then
-setUpWinbarAutoCmd()
+-- local winbarAutoCommandId = nil
+-- local setUpWinbarAutoCmd = function()
+-- 	winbarAutoCommandId = vim.api.nvim_create_autocmd(
+-- 		{ "CursorMoved", "BufWinEnter", "BufFilePost", "InsertEnter", "BufWritePost" },
+-- 		{
+-- 			callback = function()
+-- 				require("user.winbar").get_winbar()
+-- 			end,
+-- 		}
+-- 	)
 -- end
-
-vim.keymap.set("n", "yo<cr>", function()
-	if vim.api.nvim_get_option_value("winbar", { scope = "local" }) == "" then
-		setUpWinbarAutoCmd()
-		require("user.winbar").get_winbar()
-	else
-		vim.api.nvim_del_autocmd(winbarAutoCommandId)
-		pcall(vim.api.nvim_set_option_value, "winbar", "", { scope = "local" })
-	end
-end, { desc = "Toggle Winbar" })
+--
+-- -- if not vim.fn.has('gui_vimr') then
+-- setUpWinbarAutoCmd()
+-- -- end
+--
+-- vim.keymap.set("n", "yo<cr>", function()
+-- 	if vim.api.nvim_get_option_value("winbar", { scope = "local" }) == "" then
+-- 		setUpWinbarAutoCmd()
+-- 		require("user.winbar").get_winbar()
+-- 	else
+-- 		vim.api.nvim_del_autocmd(winbarAutoCommandId)
+-- 		pcall(vim.api.nvim_set_option_value, "winbar", "", { scope = "local" })
+-- 	end
+-- end, { desc = "Toggle Winbar" })
 
 vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
 	-- pattern = "*",
