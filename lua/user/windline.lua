061c65539e51f10c0aa1d2693bc253e2cbaff56b
KhalidChawtany <khalidchawtany@yahoo.com>
2023-09-13 15:37:06 +0300

    add searchindex to lua_line

--- lua/user/windline.lua
+++ lua/user/windline.lua
@@ -1,89 +1,98 @@
-local windline = require('windline')
-local helper = require('windline.helpers')
-local b_components = require('windline.components.basic')
+local windline = require("windline")
+local helper = require("windline.helpers")
+local b_components = require("windline.components.basic")
 local state = _G.WindLine.state
 
-local lsp_comps = require('windline.components.lsp')
-local git_comps = require('windline.components.git')
+local lsp_comps = require("windline.components.lsp")
+local git_comps = require("windline.components.git")
 
 local hl_list = {
-    Black = { 'white', 'black' },
-    White = { 'black', 'white' },
-    Inactive = { 'InactiveFg', 'InactiveBg' },
-    Active = { 'ActiveFg', 'ActiveBg' },
+    Black = { "white", "black" },
+    White = { "black", "white" },
+    Inactive = { "InactiveFg", "InactiveBg" },
+    Active = { "ActiveFg", "ActiveBg" },
 }
+
+
+local function printf(format, current, total)
+    if current == 0 and total == 0 then
+        return ""
+    end
+    return vim.fn.printf(format, current, total)
+end
+
 local basic = {}
 
 local breakpoint_width = 90
-basic.divider = { b_components.divider, '' }
-basic.bg = { ' ', 'StatusLine' }
+basic.divider = { b_components.divider, "" }
+basic.bg = { " ", "StatusLine" }
 
 local colors_mode = {
-    Normal = { 'red', 'black' },
-    Insert = { 'green', 'black' },
-    Visual = { 'yellow', 'black' },
-    Replace = { 'blue_light', 'black' },
-    Command = { 'magenta', 'black' },
+    Normal = { "red", "black" },
+    Insert = { "green", "black" },
+    Visual = { "yellow", "black" },
+    Replace = { "blue_light", "black" },
+    Command = { "magenta", "black" },
 }
 
 basic.vi_mode = {
-    name = 'vi_mode',
+    name = "vi_mode",
     hl_colors = colors_mode,
     text = function()
-        return { { '  ', state.mode[2] } }
+        return { { "  ", state.mode[2] } }
     end,
 }
 basic.square_mode = {
     hl_colors = colors_mode,
     text = function()
-        return { { '▊', state.mode[2] } }
+        return { { "▊", state.mode[2] } }
     end,
 }
 
 basic.lsp_diagnos = {
-    name = 'diagnostic',
+    name = "diagnostic",
     hl_colors = {
-        red = { 'red', 'black' },
-        yellow = { 'yellow', 'black' },
-        blue = { 'blue', 'black' },
+        red = { "red", "black" },
+        yellow = { "yellow", "black" },
+        blue = { "blue", "black" },
     },
     width = breakpoint_width,
     text = function(bufnr)
         if lsp_comps.check_lsp(bufnr) then
             return {
-                { lsp_comps.lsp_error({ format = '  %s', show_zero = true }), 'red' },
-                { lsp_comps.lsp_warning({ format = '  %s', show_zero = true }), 'yellow' },
-                { lsp_comps.lsp_hint({ format = '  %s', show_zero = true }), 'blue' },
+                { lsp_comps.lsp_error({ format = "  %s", show_zero = true }), "red" },
+                { lsp_comps.lsp_warning({ format = "  %s", show_zero = true }), "yellow" },
+                { lsp_comps.lsp_hint({ format = "  %s", show_zero = true }), "blue" },
             }
         end
-        return ''
+        return ""
     end,
 }
 basic.file = {
-    name = 'file',
+    name = "file",
     hl_colors = {
         default = hl_list.Black,
-        white = { 'white', 'black' },
-        magenta = { 'magenta', 'black' },
+        white = { "white", "black" },
+        magenta = { "magenta", "black" },
     },
     text = function(_, _, width)
         if width > breakpoint_width then
             return {
-                { b_components.cache_file_size(), 'default' },
-                { ' ', '' },
-                { b_components.cache_file_name('[No Name]', 'unique'), 'magenta' },
-                { b_components.line_col_lua, 'white' },
-                { b_components.progress_lua, '' },
-                { ' ', '' },
-                { b_components.file_modified(' '), 'magenta' },
+                { b_components.cache_file_size(), "default" },
+                { " ", "" },
+                { b_components.cache_file_name("[No Name]", "unique"), "magenta" },
+                { b_components.line_col_lua, "white" },
+                { b_components.progress_lua, "" },
+                { " ", "" },
+                { b_components.file_modified(" "), "magenta" },
             }
         else
             return {
-                { b_components.cache_file_size(), 'default' },
-                { ' ', '' },
-                { b_components.cache_file_name('[No Name]', 'unique'), 'magenta' },
-                { ' ', '' },
-                { b_components.file_modified(' '), 'magenta' },
+                { b_components.cache_file_size(), "default" },
+                { " ", "" },
+                { b_components.cache_file_name("[No Name]", "unique"), "magenta" },
+                { " ", "" },
+                { b_components.file_modified(" "), "magenta" },
             }
         end
     end,
@@ -91,55 +100,55 @@ basic.file = {
 basic.file_right = {
     hl_colors = {
         default = hl_list.Black,
-        white = { 'white', 'black' },
-        magenta = { 'magenta', 'black' },
+        white = { "white", "black" },
+        magenta = { "magenta", "black" },
     },
     text = function(_, _, width)
         if width < breakpoint_width then
             return {
-                { b_components.line_col_lua, 'white' },
-                { b_components.progress_lua, '' },
+                { b_components.line_col_lua, "white" },
+                { b_components.progress_lua, "" },
             }
         end
     end,
 }
 basic.git = {
-    name = 'git',
+    name = "git",
     hl_colors = {
-        green = { 'green', 'black' },
-        red = { 'red', 'black' },
-        blue = { 'blue', 'black' },
+        green = { "green", "black" },
+        red = { "red", "black" },
+        blue = { "blue", "black" },
     },
     width = breakpoint_width,
     text = function(bufnr)
         if git_comps.is_git(bufnr) then
             return {
-                { git_comps.diff_added({ format = '  %s', show_zero = true }), 'green' },
-                { git_comps.diff_removed({ format = '  %s', show_zero = true }), 'red' },
-                { git_comps.diff_changed({ format = ' 柳%s', show_zero = true }), 'blue' },
+                { git_comps.diff_added({ format = "  %s", show_zero = true }), "green" },
+                { git_comps.diff_removed({ format = "  %s", show_zero = true }), "red" },
+                { git_comps.diff_changed({ format = " 柳%s", show_zero = true }), "blue" },
             }
         end
-        return ''
+        return ""
     end,
 }
 
 local quickfix = {
-    filetypes = { 'qf', 'Trouble' },
+    filetypes = { "qf", "Trouble" },
     active = {
-        { '🚦 Quickfix ', { 'white', 'black' } },
-        { helper.separators.slant_right, { 'black', 'black_light' } },
+        { "🚦 Quickfix ", { "white", "black" } },
+        { helper.separators.slant_right, { "black", "black_light" } },
         {
             function()
                 return vim.fn.getqflist({ title = 0 }).title
             end,
-            { 'cyan', 'black_light' },
+            { "cyan", "black_light" },
         },
-        { ' Total : %L ', { 'cyan', 'black_light' } },
-        { helper.separators.slant_right, { 'black_light', 'InactiveBg' } },
-        { ' ', { 'InactiveFg', 'InactiveBg' } },
+        { " Total : %L ",                { "cyan", "black_light" } },
+        { helper.separators.slant_right, { "black_light", "InactiveBg" } },
+        { " ",                           { "InactiveFg", "InactiveBg" } },
         basic.divider,
-        { helper.separators.slant_right, { 'InactiveBg', 'black' } },
-        { '🧛 ', { 'white', 'black' } },
+        { helper.separators.slant_right, { "InactiveBg", "black" } },
+        { "🧛 ", { "white", "black" } },
     },
 
     always_active = true,
@@ -147,12 +156,12 @@ local quickfix = {
 }
 
 local explorer = {
-    filetypes = { 'fern', 'NvimTree', 'lir' },
+    filetypes = { "fern", "NvimTree", "lir" },
     active = {
-        { '  ', { 'black', 'red' } },
-        { helper.separators.slant_right, { 'red', 'NormalBg' } },
-        { b_components.divider, '' },
-        { b_components.file_name(''), { 'white', 'NormalBg' } },
+        { "  ", { "black", "red" } },
+        { helper.separators.slant_right, { "red", "NormalBg" } },
+        { b_components.divider, "" },
+        { b_components.file_name(""), { "white", "NormalBg" } },
     },
     always_active = true,
     show_last_status = true,
@@ -160,28 +169,28 @@ local explorer = {
 
 basic.lsp_name = {
     width = breakpoint_width,
-    name = 'lsp_name',
+    name = "lsp_name",
     hl_colors = {
-        magenta = { 'magenta', 'black' },
+        magenta = { "magenta", "black" },
     },
     text = function(bufnr)
         if lsp_comps.check_lsp(bufnr) then
             return {
-                { lsp_comps.lsp_name(), 'magenta' },
+                { lsp_comps.lsp_name(), "magenta" },
             }
         end
         return {
-            { b_components.cache_file_type({ icon = true }), 'magenta' },
+            { b_components.cache_file_type({ icon = true }), "magenta" },
         }
     end,
 }
 
 basic.nomodoro = {
-    name = 'nomodoro',
+    name = "nomodoro",
     hl_colors = {
-        green = { 'green', 'black' },
-        red = { 'red', 'black' },
-        blue = { 'blue', 'black' },
+        green = { "green", "black" },
+        red = { "red", "black" },
+        blue = { "blue", "black" },
     },
     width = breakpoint_width,
     text = function(bufnr)
@@ -192,8 +201,41 @@ basic.nomodoro = {
     end,
 }
 
+basic.search_count = {
+    name = "search_count",
+    hl_colors = {
+        green = { "green", "black" },
+        red = { "red", "black" },
+        blue = { "blue", "black" },
+    },
+    width = breakpoint_width,
+    text = function(bufnr)
+        if vim.v.hlsearch == 0 then
+            return ""
+        end
+        vim.fn.searchcount { recompute = 1, maxcount = 0, timeout = 100 }
+        local result = vim.fn.searchcount({ recompute = 0 })
+        if vim.tbl_isempty(result) then
+            return ""
+        end
+        ---NOTE: the search term can be included in the output
+        --- using [%s] but this value seems flaky
+        -- local search_reg = fn.getreg("@/")
+        if result.incomplete == 1 then -- timed out
+            return printf(" ?/?? ")
+        elseif result.incomplete == 2 then -- max count exceeded
+            if result.total > result.maxcount and result.current > result.maxcount then
+                return printf(" >%d/>%d ", result.current, result.total)
+            elseif result.total > result.maxcount then
+                return printf(" %d/>%d ", result.current, result.total)
+            end
+        end
+        return printf(" %d/%d ", result.current, result.total)
+    end,
+}
+
 local default = {
-    filetypes = { 'default' },
+    filetypes = { "default" },
     active = {
         basic.square_mode,
         basic.vi_mode,
@@ -201,12 +243,13 @@ local default = {
         basic.lsp_diagnos,
         basic.divider,
         basic.nomodoro,
+        basic.search_count,
         basic.divider,
         basic.file_right,
         basic.lsp_name,
         basic.git,
-        { git_comps.git_branch(), { 'magenta', 'black' }, breakpoint_width },
-        { ' ', hl_list.Black },
+        { git_comps.git_branch(), { "magenta", "black" }, breakpoint_width },
+        { " ",                    hl_list.Black },
         basic.square_mode,
     },
     inactive = {
@@ -214,8 +257,8 @@ local default = {
         basic.file_name_inactive,
         basic.divider,
         basic.divider,
-        { b_components.line_col, hl_list.Inactive },
-        { b_components.progress, hl_list.Inactive },
+        { b_components.line_col,       hl_list.Inactive },
+        { b_components.progress,       hl_list.Inactive },
     },
 }
 
@@ -232,24 +275,19 @@ windline.setup({
     },
 })
 
-
 local function reset_windline_bg()
-
     -- get set colors
     local highlights = vim.api.nvim__get_hl_defs(0)
 
     -- remove empty names
-    vim.tbl_filter(
-        function(key)
-            return type(key) == "string" and #key > 0
-        end,
-        highlights
-    )
+    vim.tbl_filter(function(key)
+        return type(key) == "string" and #key > 0
+    end, highlights)
 
     for name, highlight in pairs(highlights) do
         if type(name) == "string" and #name > 0 then
-            if (name:startswith('WL')) then
-                vim.cmd('hi ' .. name .. ' guibg=#111827')
+            if name:startswith("WL") then
+                vim.cmd("hi " .. name .. " guibg=#111827")
             end
         end
     end
