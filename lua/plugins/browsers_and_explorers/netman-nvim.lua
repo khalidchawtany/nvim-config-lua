674c339dd14c30548377542e5df187bc5844b47f
KhalidChawtany <khalidchawtany@yahoo.com>
2024-03-13 12:09:00 +0300

    remove netman triptych and better-whitespace

--- lua/plugins/browsers_and_explorers/netman-nvim.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "miversen33/netman.nvim", -- allow ftp and docker
-  dependencies = {"nvim-neo-tree/neo-tree.nvim"},
-  branch = "v1.1",
-  cmd = {"NmloadProvider"},
-  config = function()
-    require("netman")
-  end
-}
