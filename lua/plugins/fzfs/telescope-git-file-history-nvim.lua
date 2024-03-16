d94ff82391d11bdf374b8824e0e0ccf7df17e003
KhalidChawtany <khalidchawtany@yahoo.com>
2024-03-16 23:40:32 +0300

    install telescope-git-file-history

---
+++ lua/plugins/fzfs/telescope-git-file-history-nvim.lua
@@ -0,0 +1,35 @@
+return {
+
+    "isak102/telescope-git-file-history.nvim",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "tpope/vim-fugitive"
+    },
+    keys = {
+        {
+            "<c-s><c-cr>",
+            function()
+                require("telescope").extensions.git_file_history.git_file_history()
+            end,
+            desc = "Telescope git_file_history"
+        }
+    },
+    config = function()
+        local gfh_actions = require("telescope").extensions.git_file_history.actions
+        require("telescope").setup {
+            extensions = {
+                git_file_history = {
+                    mappings = {
+                        i = {
+                            ["<C-g>"] = gfh_actions.open_in_browser,
+                        },
+                        n = {
+                            ["<C-g>"] = gfh_actions.open_in_browser,
+                        },
+                    },
+                },
+            },
+        }
+        require("telescope").load_extension("git_file_history")
+    end
+}
