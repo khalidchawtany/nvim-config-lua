fac9e14b7c58debf1dc1ac722f1f1c1f51b17437
KhalidChawtany <khalidchawtany@yahoo.com>
2025-03-15 11:59:27 +0300

    remove my implementation of bigfile

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,33 +1,3 @@
-vim.cmd([[
-" autocmd BufReadPost * silent! :%foldopen!
-
- " disable syntax highlighting in big files
-function DisableSyntaxTreesitter()
-    echo("Big file, disabling syntax, treesitter and folding")
-    if exists(':TSBufDisable')
-        exec 'TSBufDisable autotag'
-        exec 'TSBufDisable highlight'
-        " etc...
-    endif
-
-    set foldmethod=manual
-    syntax clear
-    syntax off    " hmmm, which one to use?
-    filetype off
-    set noundofile
-    set noswapfile
-    set noloadplugins
-endfunction
-
-augroup BigFileDisable
-    autocmd!
-    " autocmd BufWinEnter * if getfsize(expand("%")) > 512 * 1024 | exec DisableSyntaxTreesitter() | endif
-    autocmd BufReadPre,FileReadPre * if getfsize(expand("%")) > 512 * 1024 | exec DisableSyntaxTreesitter() | endif
-
-augroup END
-
-
-]])
 
 require("user.autocmd_buffer_clean")
 
